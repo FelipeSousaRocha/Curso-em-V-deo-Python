cab63c336fc13f955eee686f9f451a54b73dfe0c
FelipeSousaRocha <77278366+FelipeSousaRocha@users.noreply.github.com>
2021-01-22 11:52:33 -0300

    Add files via upload

---
+++ Primeiro Mundo/aula11 cores.py
@@ -0,0 +1,12 @@
+#cor em python ANSI
+#\033[style;text;backm
+#\033[0;33;44m
+#style 0 none,1 bold,4 underline,7 negative
+#text 30 branco,31 vermelho,32 verde,33 amarelo,34 azul,35 rosa,36 ciano,37 cinza
+#back 40,41,42,43,44,45,46
+\033[0;30;41m
+\033[4;33;44m
+\033[1;35;43m
+\033[0;30;42m
+\033[m
+\033[7;30m
---
+++ Primeiro Mundo/aula11p2.py
@@ -0,0 +1,5 @@
+print('\033[33mOlá Mundo')
+print('\033[34;40mOlá Mundo\033[m') #background só no nome
+print('\033[30mOlá Mundo')
+print('\033[7;30mOlá Mundo\033[m')
+cores = [] #lista de cores
