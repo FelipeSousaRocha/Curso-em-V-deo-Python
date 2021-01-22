f4fdb2edc07a7d03634d5c31ef4731be248dc71a
FelipeSousaRocha <77278366+FelipeSousaRocha@users.noreply.github.com>
2021-01-22 11:51:36 -0300

    Delete desafio 050.py

--- desafio 050.py
+++
@@ -1,8 +0,0 @@
-soma = 0
-cont = 0
-for c in range(1, 7):
-    num = int(input('Digite o {} número: '.format(c)))
-    if num % 2 == 0:
-        soma += num
-        cont += cont + 1
-print('A soma de todos os {} números pares dos 6 digitados é {}'.format(cont,soma))
