9b5cd2afa794cafbcfe82259e3d63d8da6fca265
FelipeSousaRocha <77278366+FelipeSousaRocha@users.noreply.github.com>
2021-01-22 11:51:48 -0300

    Delete desafio 048.py

--- desafio 048.py
+++
@@ -1,7 +0,0 @@
-soma = 0
-cont = 0
-for c in range(1,501):
-    if c%3 == 0 and c%2!=0 or c%3==3 :
-        soma = soma + c
-        cont = cont+1
-print('A soma de todos os {} valores solicitados é {}'.format(cont,soma))
