e57f37f851936b806f64e974173a35591867d5e4
FelipeSousaRocha <77278366+FelipeSousaRocha@users.noreply.github.com>
2021-01-22 11:48:44 -0300

    Delete desafio 64.py

--- desafio 64.py
+++
@@ -1,8 +0,0 @@
-n = cont = soma = 0
-n = int(input('Digite um número: '))
-while n != 999:
-    soma += n
-    cont += 1
-    n = int(input('Digite um número: '))
-#print('acabou voce digitou {} numeros e a soma dos números é {}'.format(cont -1,soma-999))
-print('acabou voce digitou {} numeros e a soma dos números é {}'.format(cont -1,soma))
