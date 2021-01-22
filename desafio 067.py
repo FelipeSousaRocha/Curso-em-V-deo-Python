344cbec2a393f735dcb266942a36aa9c80f0c830
FelipeSousaRocha <77278366+FelipeSousaRocha@users.noreply.github.com>
2021-01-22 11:50:02 -0300

    Delete desafio 067.py

--- desafio 067.py
+++
@@ -1,8 +0,0 @@
-c = 0
-while True:
-    n = int(input('Digite a tabuada que você deseja ver[digite número negativo para parar]: '))
-    if n < 0:
-        break
-    for c in range(0, 11):
-        print(f'{n} x {c} = {c*n}')
-
