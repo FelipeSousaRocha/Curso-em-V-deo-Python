4a302579682909e0b8cf68ea75adb7744fd35409
FelipeSousaRocha <77278366+FelipeSousaRocha@users.noreply.github.com>
2021-01-22 11:50:09 -0300

    Delete desafio 066.py

--- desafio 066.py
+++
@@ -1,8 +0,0 @@
-c = s = 0
-while True:
-    n = int(input("Digite o valor [Para parar digite 999] : "))
-    if n == 999:
-        break
-    s += n
-    c += 1
-print(f'A soma dos {c} valores é {s}')
